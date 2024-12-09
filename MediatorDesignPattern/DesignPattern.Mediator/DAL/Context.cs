eb85adac0d7404e4b0a2375e74cb571972c918b4
Deha Edebali Çapar <dehaedebali37@gmail.com>
2024-12-09 22:56:10 +0300

    Add Mediator DesignPattern

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Mediator.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-04LJ0NG\\SQLEXPRESS; initial catalog=MediatorDesignPattern; integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
