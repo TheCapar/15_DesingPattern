7254413b7a95f77c9159a920fe58a3e5d8d388d6
Deha Edebali Çapar <dehaedebali37@gmail.com>
2024-12-10 17:54:19 +0300

    Add Iterator Design Pattern

---
+++ IteratorDesignPattern/DesignPattern.Itarator/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using DesignPattern.Iterator.IteratorPattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Iterator.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            VisitRouteMover visitRouteMover = new VisitRouteMover();
+            List<string> strings = new List<string>();
+            visitRouteMover.AddVisitRoute(new VisitRoute
+            {
+                Country = "Almanya",
+                CityName ="Berlin",
+                VisitPlaceName = "Berlin Kapısı"
+            });
+            visitRouteMover.AddVisitRoute(new VisitRoute
+            {
+                Country = "Fransa",
+                CityName = "Paris",
+                VisitPlaceName = "Eyfel Kulesi"
+            });
+            visitRouteMover.AddVisitRoute(new VisitRoute
+            {
+                Country = "İtalya",
+                CityName = "Venedik",
+                VisitPlaceName = "Gondol"
+            });
+            visitRouteMover.AddVisitRoute(new VisitRoute
+            {
+                Country = "İtalya",
+                CityName = "Roma",
+                VisitPlaceName = "Colesyum"
+            });
+            visitRouteMover.AddVisitRoute(new VisitRoute
+            {
+                Country = "Çek Cumhuriyeti",
+                CityName = "Prag",
+                VisitPlaceName = "Meydan"
+            });
+
+            var iterator = visitRouteMover.CreateIterator();
+
+            while (iterator.NextLocation())
+            {
+                strings.Add(iterator.CurrentItem.Country+" "+iterator.CurrentItem.CityName+" "+iterator.CurrentItem.VisitPlaceName);
+            }
+            ViewBag.v = strings;
+            return View();
+        }
+    }
+}
